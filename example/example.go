4ee20f81f3b1936eea970170fb91c64699262de8
fengyang <99>
2023-05-11 14:56:44 +0800

    Initial commit

---
+++ example/example.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	i18n "github.com/fy976061258/gin-i18n"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/text/language"
+)
+
+func setupRouter(bundle *i18n.Bundle) *gin.Engine {
+	r := gin.Default()
+
+	//r.Use(i18n.Serve(bundle))
+	r.GET("/ping", func(c *gin.Context) {
+
+		text := i18n.FormatMessage(c,
+			&i18n.Message{
+				ID:    "ping-text",
+				Other: "你好,{{.who}}",
+			},
+			map[string]interface{}{
+				"who": "gin",
+			})
+
+		c.String(200, text)
+	})
+	r.GET("/ping2", func(c *gin.Context) {
+		c.String(200, i18n.FormatMessage(c, &i18n.Message{ID: "ping2-text", Other: "我是{{.who}}"}, i18n.Data{"who": "gin"}))
+	})
+	r.GET("/ping3", func(c *gin.Context) {
+		c.String(200, i18n.FormatText(c, &i18n.Message{ID: "ping3-text", Other: "测试"}))
+	})
+
+	return r
+}
+
+func main() {
+
+	bundle := i18n.NewBundle(
+		language.Chinese,
+		"example/active.zh-CN.toml",
+		"example/active.en-US.toml",
+		"example/active.ja-JP.toml",
+	)
+
+	r := setupRouter(bundle)
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
---
+++ example/example_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	i18n "github.com/fy976061258/gin-i18n"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/text/language"
+)
+
+func TestPingRoute(t *testing.T) {
+	bundle := i18n.NewBundle(
+		language.Chinese,
+		"active.zh-CN.toml",
+		"active.en-US.toml",
+		"active.ja-JP.toml",
+	)
+
+	router := setupRouter(bundle)
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/ping", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "你好,gin", w.Body.String())
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/ping?lang=en-US", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "hello,gin", w.Body.String())
+
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/ping?lang=ja-JP", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, "こんにちは,gin", w.Body.String())
+}
